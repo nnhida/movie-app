f101ced805fa58235c35b481278957388df9b615
nnhida <nurhidayahayundira296@gmail.com>
2024-12-23 13:58:49 +0700

    init

---
+++ src/api/reviews.route.js
@@ -0,0 +1,10 @@
+import express from 'express';
+import ReviewsController from './reviews.controller.js';
+
+const router = express.Router();
+
+router.route('/movie/:id').get(ReviewsController.apiGetReviews);
+router.route('/new').post(ReviewsController.apiPostReview);
+router.route('/:id').get(ReviewsController.apiGetReview).put(ReviewsController.apiUpdateReview).delete(ReviewsController.apiDeleteReview);
+
+export default router;
---
+++ src/index.js
@@ -0,0 +1,27 @@
+import 'dotenv/config';
+import app from './server.js';
+import mongodb from 'mongodb';
+import ReviewDAO from './dao/reviewsDAO.js';
+
+const MongoClient = mongodb.MongoClient;
+const mongo_username = process.env['MONGO_USERNAME'];
+const mongo_password = process.env['MONGO_PASSWORD'];
+const uri = `mongodb+srv://${mongo_username}:${mongo_password}@cluster0.1fnmu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+const port = 80;
+
+MongoClient.connect(uri, {
+  maxPoolSize: 50,
+  wtimeoutMS: 2500,
+  // useNewUrlParser: true,
+})
+  .catch((err) => {
+    console.error(err.stack);
+    process.exit(1);
+  })
+  .then(async (client) => {
+    await ReviewDAO.injectDB(client);
+    app.listen(port, () => {
+      console.log(`listening on port ${port}`);
+    });
+  });
